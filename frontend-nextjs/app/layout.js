8a6f1963dab4633cf0eed5a13ed7d34cf06c9539
Bilal Toor <bilaltoor786786@gmail.com>
2025-10-03 00:28:49 -0700

    fdasf

--- frontend-nextjs/app/layout.js
+++ frontend-nextjs/app/layout.js
@@ -59,6 +59,9 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="kjnhCyDgoyle5lHzsEBs87ilZPT2tti_OaV_N-YjwvQ" />
+      </head>
       <body>
         {children}
         {/* Global JSON-LD */}
